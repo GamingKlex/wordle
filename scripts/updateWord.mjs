661ae5cf75b8003f911cfe60a09cc7abfb90dc57
GamingKlex <gamingklexmail@gmail.com>
2025-01-04 09:58:35 +0100

    ✨ Add updateWord script for later action

---
+++ scripts/updateWord.mjs
@@ -0,0 +1,27 @@
+// This script is run every 24 hours to update today's and yesterday's word.
+
+import fs from "fs";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const yesterdayFile = path.join(__dirname, "../public/YESTERDAY");
+const todayFile = path.join(__dirname, "../public/TODAY");
+
+console.log("Updating yesterday's word...");
+
+const yesterdayWord = fs.readFileSync(todayFile, "utf-8");
+fs.cpSync(todayFile, yesterdayFile, { force: true });
+
+console.log("Updating today's word...");
+
+const words = fs
+  .readFileSync(path.join(__dirname, "../wordlist.txt"), "utf-8")
+  .split("\n")
+  .filter((word) => word !== yesterdayWord);
+const word = words[Math.floor(Math.random() * words.length)];
+
+fs.writeFileSync(todayFile, word.toUpperCase());
+
+console.log("Done!");
