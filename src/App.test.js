606c8bd9435f0a1a7a4a9e12e516f022d4cdd5f7
GamingKlex <gamingklexmail@gmail.com>
2025-01-04 12:47:33 +0100

    ✅ Add simple tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,11 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders", () => {
+test("renders game", () => {
   render(<App />);
+  const titleElement = screen.getByText(/Wordle/i);
+  expect(titleElement).toBeInTheDocument();
+
+  const howToPlayButton = screen.getByText(/How to play/i);
+  expect(howToPlayButton).toBeInTheDocument();
 });
