917077ae0009bd64852ffc9a29a673976e5ce7fe
GamingKlex <gamingklexmail@gmail.com>
2025-01-04 01:51:19 +0100

    💄 Design sketch

--- README.md
+++ README.md
@@ -4,5 +4,5 @@ A simple clone of the popular wordle game!
 
 # How does this word:
 
-- [create-react-app](https://create-react-app.dev/) as base!
-- [Lucide](https://lucide.dev/) for icons!
+- [create-react-app](https://create-react-app.dev/) as a base!
+- [Lucide](https://lucide.dev/) for the icons!
--- src/App.js
+++ src/App.js
@@ -8,10 +8,34 @@ function App() {
       <div className="container">
         <div className="title">Wordle</div>
         <div className="subtitle">Guess todays word!</div>
+
         <button onClick={() => setDialogOpen(true)} className="howtoplay-btn">
           <CircleHelp size={18} />
           How to play
         </button>
+
+        <div className="game-container">
+          <GuessedRow
+            letters={[
+              { value: "A", type: "right" },
+              { value: "B", type: "wrong" },
+              { value: "C", type: "wrongpos" },
+              { value: "D", type: "right" },
+              { value: "E", type: "wrong" },
+            ]}
+          />
+          <GuessingRow />
+          <BlankRow />
+          <BlankRow />
+          <BlankRow />
+          <BlankRow />
+        </div>
+
+        <div className="note">
+          Yesterday's word was <span className="note-word">APPLE</span>{" "}
+          <span style={{ margin: "0 0.5rem" }}>—</span> New word in 3 min. and
+          20 seconds!
+        </div>
       </div>
       {dialogOpen && <Dialog onClose={() => setDialogOpen(false)} />}
     </>
@@ -41,6 +65,52 @@ function Dialog({ onClose }) {
           est, quin suam vim retineat a primo ad extremum.
         </div>
       </div>
+    </div>
+  );
+}
+
+function GuessedRow({ letters }) {
+  return (
+    <div className="row">
+      {letters.map((letter, i) => (
+        <div key={i} className={"guessed-letter " + letter.type}>
+          {letter.value}
+        </div>
+      ))}
+    </div>
+  );
+}
+
+function GuessingRow() {
+  return (
+    <div className="row">
+      <div className="guessing-letter">
+        <input />
+      </div>
+      <div className="guessing-letter">
+        <input />
+      </div>
+      <div className="guessing-letter">
+        <input />
+      </div>
+      <div className="guessing-letter">
+        <input />
+      </div>
+      <div className="guessing-letter">
+        <input />
+      </div>
+    </div>
+  );
+}
+
+function BlankRow() {
+  return (
+    <div className="row">
+      <div className="blank-letter"></div>
+      <div className="blank-letter"></div>
+      <div className="blank-letter"></div>
+      <div className="blank-letter"></div>
+      <div className="blank-letter"></div>
     </div>
   );
 }
